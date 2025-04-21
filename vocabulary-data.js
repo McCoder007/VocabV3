9c0410a5e0cee6fe8021688ecdf7dd433bbb980b
McCoder007 <shawn@mcgrathplace.com>
2025-04-20 22:12:08 -0700

    Update vocabulary with new set of words and images

--- vocabulary-data.js
+++ vocabulary-data.js
@@ -1,32 +1,98 @@
 const vocabularyData = [
     {
-        image: 'images/cat.png',
-        english: 'cat',
-        chinese: '猫',
+        image: 'images/headphones.png',
+        english: 'headphones',
+        chinese: '耳机',
         status: 'new'
     },
     {
-        image: 'images/dog.png',
-        english: 'dog',
-        chinese: '狗',
+        image: 'images/basketball.png',
+        english: 'basketball',
+        chinese: '篮球',
         status: 'new'
     },
     {
-        image: 'images/book.png',
-        english: 'book',
-        chinese: '书',
+        image: 'images/ski.png',
+        english: 'skiing',
+        chinese: '滑雪',
         status: 'new'
     },
     {
-        image: 'images/tree.png',
-        english: 'tree',
-        chinese: '树',
+        image: 'images/windsurfing.png',
+        english: 'windsurfing',
+        chinese: '帆板运动',
         status: 'new'
     },
     {
-        image: 'images/sun.png',
-        english: 'sun',
-        chinese: '太阳',
+        image: 'images/rollercoaster.png',
+        english: 'roller coaster',
+        chinese: '过山车',
+        status: 'new'
+    },
+    {
+        image: 'images/tree_roots.png',
+        english: 'tree roots',
+        chinese: '树根',
+        status: 'new'
+    },
+    {
+        image: 'images/lungs.png',
+        english: 'lungs',
+        chinese: '肺',
+        status: 'new'
+    },
+    {
+        image: 'images/mud.png',
+        english: 'mud',
+        chinese: '泥',
+        status: 'new'
+    },
+    {
+        image: 'images/ladder.png',
+        english: 'ladder',
+        chinese: '梯子',
+        status: 'new'
+    },
+    {
+        image: 'images/duck.png',
+        english: 'duck',
+        chinese: '鸭子',
+        status: 'new'
+    },
+    {
+        image: 'images/cliff_diving.png',
+        english: 'cliff diving',
+        chinese: '悬崖跳水',
+        status: 'new'
+    },
+    {
+        image: 'images/button.png',
+        english: 'button',
+        chinese: '按钮',
+        status: 'new'
+    },
+    {
+        image: 'images/sewing_machine.png',
+        english: 'sewing machine',
+        chinese: '缝纫机',
+        status: 'new'
+    },
+    {
+        image: 'images/backpack.png',
+        english: 'backpack',
+        chinese: '背包',
+        status: 'new'
+    },
+    {
+        image: 'images/train.png',
+        english: 'train',
+        chinese: '火车',
+        status: 'new'
+    },
+    {
+        image: 'images/windmill.png',
+        english: 'windmill',
+        chinese: '风车',
         status: 'new'
     }
 ]; 
